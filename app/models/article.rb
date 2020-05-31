ebba9b343e42964cb04ab4a163c5e4613a2271a7
ramon godas <ramonunivigo@gmail.com>
2020-05-31 00:54:01 -0300

    testing validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord 
-    
+    # VALIDATIONS We need to put the restriction to create the article
+    validates :title, presence: true, length: {minimum: 6, maximum: 100}
+    validates :description, presence: true, length: {minimum:10, maximum: 300}
 end
