f7c9c8d3a41b20066f377032c5326b264234edd0
ramon godas <ramonunivigo@gmail.com>
2020-06-02 11:58:37 -0300

    Add users table and user model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
     # VALIDATIONS We need to put the restriction to create the article
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum:10, maximum: 300}
+
+    belongs_to :user
 end
