83e19561bde4eeed72b74cff13477e08df035638
ramon godas <ramonunivigo@gmail.com>
2020-06-06 21:56:23 -0300

    add test new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,39 @@
 class CategoriesController < AcpplicationController
-    
+    before_Action :require_admin, except: [:index, :show]
+
     def new
-    
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.after_save 
+            flash[:notice] = "Category was created"
+            redirect_to @category
+        else
+            render 'new'
+        end
     end
 
     def index
-    
+        @categories = Category.paginate(page: params[:page], per_page: 4)
     end
 
     def show
-    
+        @category = Category.find(params[:id])
     end
 
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if (logged_in? && current_user.admin?)
+            flash[:alert] "Only admin can perform such an action"
+            redirect_to categories_path
+        end
+    end
+    
 end
