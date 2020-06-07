e9dad513d8aaeffe36b89b14656aba4dc8c06610
ramon godas <ramonunivigo@gmail.com>
2020-06-07 00:51:34 -0300

    add too many asso article_categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,6 @@ class Article < ApplicationRecord
     validates :description, presence: true, length: {minimum:10, maximum: 300}
 
     belongs_to :user
+    has_many :article_categories
+    has_many :categories, throught: :article_categories
 end
